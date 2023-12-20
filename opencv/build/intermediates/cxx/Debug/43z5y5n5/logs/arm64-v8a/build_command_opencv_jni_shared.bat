6db22d0bb12a6bbd76e66d9aee9a7be10697420e
jiejanezhang <jie.jane.zhang@intel.com>
2023-12-20 17:30:10 +0800

    Initial commit

---
+++ opencv/build/intermediates/cxx/Debug/43z5y5n5/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\jzhan66\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\jzhan66\\AndroidStudioProjects\\MeasureSizeByOpenCV\\opencv\\.cxx\\Debug\\43z5y5n5\\arm64-v8a" ^
+  opencv_jni_shared
