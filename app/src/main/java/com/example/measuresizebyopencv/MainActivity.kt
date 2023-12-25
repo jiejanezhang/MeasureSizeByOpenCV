c6abf540b50025f93015dc0f6f0b44dfb138560b
jiejanezhang <jie.jane.zhang@intel.com>
2023-12-25 15:13:39 +0800

    Experiment #07 Part1
    Remove the sharp angles. Part 1.

--- app/src/main/java/com/example/measuresizebyopencv/MainActivity.kt
+++ app/src/main/java/com/example/measuresizebyopencv/MainActivity.kt
@@ -15,6 +15,9 @@ import org.opencv.android.Utils
 import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.*
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
@@ -119,6 +122,86 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    // Process contour:
+    // 1. Detect the convex points with bounding box.
+    // 2. If it is sharp angle, remove the convex.
+    private fun processContour(contour : MatOfPoint, origMat: Mat, toDrawPoly: Boolean, toDrawResult: Boolean): Boolean  {
+        // approximate a contour shape
+        val contour2f = MatOfPoint2f()
+        contour.convertTo(contour2f, CvType.CV_32FC2)
+        val epsilon = 0.01 * arcLength(contour2f, true)
+        val approxContour2f = MatOfPoint2f()
+        approxPolyDP(contour2f, approxContour2f, epsilon, true)
+        val curve = MatOfPoint()
+        approxContour2f.convertTo(curve, CvType.CV_32S)
+        val curveList = ArrayList<MatOfPoint>()
+        curveList.add(curve)
+
+        if (toDrawPoly) drawContours(origMat, curveList, 0, Scalar(255.0, 255.0, 0.0), 5)
+
+        // Find the binding box Rect of approx Poly DP
+        val boundRect: Rect = boundingRect(curve)
+        rectangle(origMat, boundRect.tl(), boundRect.br(), Scalar(255.0, 0.0, 255.0), 5, 8, 0);
+        println("Bounding Box tl: [${boundRect.tl().x} ${boundRect.tl().y}]   br: [${boundRect.br().x} ${boundRect.br().y}]")
+
+        // Judge the bounding points
+        val pointList = curve.toList()
+        var newPointList: MutableList<Point> = mutableListOf()
+        var updated = false
+        for ( (index, point) in pointList.withIndex()) {
+            println("$index: [${point.x} ${point.y}]")
+            var textPoint = Point(point.x-10, point.y+20)
+            putText(origMat,index.toString(), textPoint, FONT_HERSHEY_PLAIN, 10.0, Scalar(0.0, 0.0, 0.0), 8)
+
+            // Check if the point is in bounding box
+            if (point.x in boundRect.tl().x - 2 .. boundRect.tl().x + 2 ||
+                point.x in boundRect.br().x - 2 .. boundRect.br().x + 2 ||
+                point.y in boundRect.tl().y - 2 .. boundRect.tl().y + 2 ||
+                point.y in boundRect.br().y - 2 .. boundRect.br().y + 2 )
+            {
+                println("Point is in bounding box")
+                //circle(origMat, point, 100, Scalar(0.0, 0.0, 0.0), 5)
+                val preIndex = if (index == 0)  pointList.size - 1 else index - 1
+                val nextIndex = if (index == pointList.size -1 )  0 else index + 1
+                val prePoint = pointList[preIndex]
+                val nextPoint = pointList[nextIndex]
+                // 邻边a长度平方
+                val aSquare = (prePoint.x - point.x).pow(2) +
+                        (prePoint.y - point.y).pow(2)
+                // 邻边b长度平方
+                val bSquare = (nextPoint.x - point.x).pow(2) +
+                        (nextPoint.y - point.y).pow(2)
+                // 对边c长度平方
+                val cSquare = (nextPoint.x - prePoint.x).pow(2) + (nextPoint.y - prePoint.y).pow(2)
+                println("Previous Point is $preIndex. Next Point is $nextIndex")
+
+                // 如果形成锐角则是毛刺，去掉。
+                if ( cSquare < aSquare + bSquare ) {
+                    println("Remove this point.")
+                    //circle(origMat, point, 100, Scalar(255.0, 0.0, 0.0), 5)
+                    updated = true
+                }
+                else {
+                    println("Keep this point.")
+                    newPointList.add(point)
+                }
+            }
+            else {
+                println("Keep this point.")
+                newPointList.add(point)
+            }
+        }
+
+        // Show the new contour with the updated pointList
+        if (updated) {
+            contour.fromArray(*newPointList.toTypedArray())
+            curveList.removeAt(0)
+            curveList.add(contour)
+            if (toDrawResult) drawContours(origMat, curveList, 0, Scalar(255.0, 255.0, 255.0), 9)
+        }
+        return updated
+    }
+
     private fun showImage(imgMat: Mat)
     {
         // Create a new Bitmap named bwBitmap to hold the filtered image
@@ -175,31 +258,22 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        // Call approxPolyDP() to approximate a contour shape and draw it
-        val contour = contours[maxIdx]
-        val contour2f = MatOfPoint2f()
-        contour.convertTo(contour2f, CvType.CV_32FC2)
-        val approxContour2f = MatOfPoint2f()
-        val epsilon = 0.007 * arcLength(contour2f, true)
-        approxPolyDP(contour2f, approxContour2f, epsilon, true)
-        val curve = MatOfPoint()
-        approxContour2f.convertTo(curve, CvType.CV_32S)
-        val curveList = ArrayList<MatOfPoint>()
-        curveList.add(curve)
-
-        when (filterOption) {
-            "Contour Only" -> {
-                drawContours(origMat, contours, maxIdx, Scalar(255.0, 0.0, 0.0), 15)
-            }
-            "Approx Poly Only" -> {
-                drawContours(origMat, curveList, 0, Scalar(0.0, 255.0, 255.0), 15)
-
-            }
+        var toDrawResult = false
+        var toDrawPoly = false
+        when (filterOption)
+        {
+            "Processed Curve Only" -> toDrawResult = true
+            "Approx Poly Only" -> toDrawPoly = true
             "Both" -> {
-                drawContours(origMat, contours, maxIdx, Scalar(255.0, 0.0, 0.0), 15)
-                drawContours(origMat, curveList, 0, Scalar(0.0, 255.0, 255.0), 15)
+                toDrawResult = true
+                toDrawPoly = true
             }
         }
+        drawContours(origMat, contours, maxIdx, Scalar(255.0, 0.0, 0.0), 15)
+
+        // Call approxPolyDP() to approximate a contour shape and draw it
+        val contour = contours[maxIdx]
+        processContour(contour, origMat, toDrawPoly, toDrawResult)
 
         showImage(origMat)
 
